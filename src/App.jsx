fb302140fc0f3db4072afc5eb0c9e285f2aeabdd
Job Natanael Contreras Garzon <143237173+Job-Natanael-Contreras-Garzon@users.noreply.github.com>
2025-04-18 15:24:04 -0400

    create home header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
-import Header from "./components/Header/Header"; 
+
 import "./app.css";
+//import Home_Header from "./components/home_header/Home_Header";
+import Second_Header from "./components/second_header/Second_Header";
 function App() {
   return (
     <BrowserRouter>
-    
-    <div className="app-container">
-    <Header />
-      <video autoPlay loop muted playsInline className="background-video">
-        <source
-          src="https://res.cloudinary.com/dwzjheic7/video/upload/v1744945255/WhatsApp_Video_2025-04-17_at_23.00.21_utueeb.mp4"
-          type="video/mp4"
-        />
-      </video>
-    </div>
+      <div className="app-container">
+        <Second_Header />
+        <video autoPlay loop muted playsInline className="background-video">
+          <source
+            src="https://res.cloudinary.com/dwzjheic7/video/upload/v1744945255/WhatsApp_Video_2025-04-17_at_23.00.21_utueeb.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
       {/* Contenido de la app */}
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
