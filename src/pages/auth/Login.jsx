f123d9c81bd9455cd065453892de14c64e827c64
MiguelAGutierrezSantalla <gutierrezsantallamiguelangel@gmail.com>
2025-04-18 12:25:53 -0600

    añadiendo formulario de inicio de sision y de registro

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import './../../styles/pages/auth/login.css';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+
+    <div className="conteiner-login-component">
+
+      <div className="background-video">
+        <video autoPlay loop muted >
+          <source src="https://cdn.pixabay.com/video/2020/10/19/52849-473336269_large.mp4" type="video/mp4" />
+          Tu navegador no soporta el video.
+        </video>
+      </div>
+ 
+      <div className="login-container">
+        <div className="login-form">
+          <h2>Bienvenido</h2>
+          <form>
+              <div className="conteiner-input-login">
+                <label htmlFor="email-login">Correo electrónico</label>
+                <input type="email" id="email-login" required />
+                
+                <label htmlFor="password-login">Contraseña</label>
+                <input type="password" id="password-login" required />
+              </div>
+              <button type="submit">Iniciar Sesión</button>
+          </form>
+
+          <div className="links">
+            <p>¿No tienes una cuenta?</p>
+            <Link to="/register">¡Crea una!</Link>
+          </div>
+        </div>
+
+        <div className="image-container">
+          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARYAAAC1CAMAAACtbCCJAAAAA1BMVEWUlZnYhXhJAAAASElEQVR4nO3BAQ0AAADCoPdPbQ43oAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIBjA8VDAAH+iSvZAAAAAElFTkSuQmCC" alt="Imagen de fondo" />
+        </div>
+      </div>
+    </div>
+  );
 };
+
 
 export default Login;
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import './../../styles/pages/auth/register.css';
 
 const Register = () => {
-  return <div>Register</div>;
+  return(
+    
+    <div className="conteiner-register-component">
+      <div className="background-video">
+        <video autoPlay loop muted >
+          <source src="https://cdn.pixabay.com/video/2020/10/19/52849-473336269_large.mp4" type="video/mp4" />
+          Tu navegador no soporta el video.
+        </video>
+      </div>
+ 
+      <div className="register-container">
+        <div className="register-form">
+          <h2>Registrate</h2>
+          <form>
+              <div className="conteiner-input-register">
+                <label htmlFor="email-register">Correo electrónico</label>
+                <input type="email" id="email-register" required />
+
+                <label htmlFor="phote-register">Numero de celular</label>
+                <input type="tel" id="phote-register" required />
+
+                <label htmlFor="name-register">Nombre completo</label>
+                <input type="text" id="name-register" required />
+
+                <label htmlFor="password-register">Contraseña</label>
+                <input type="password" id="password-register" required />
+              </div>
+              <button type="submit">Registrase</button>
+          </form>
+
+          <div className="links">
+            <p>¿Ya tienes una cuenta?</p>
+            <Link to="/login">! Inicia sesión!</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
