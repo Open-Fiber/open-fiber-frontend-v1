ea6f163b03ce52c8fa11c4bc6f0b165ee1804c87
Maru <marcelo0920.z@gmail.com>
2025-04-19 07:11:24 -0400

    hot fix

--- src/components/home/CategoryGrid.jsx
+++ src/components/home/CategoryGrid.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CategoryCard from "./CategoryCard";
-import "./styles/CategoryGrid.css";
+import "./styles/categorygrid.css";
 
 const CategoryGrid = () => {
   const categories = [
