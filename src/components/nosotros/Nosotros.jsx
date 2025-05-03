521a55f6cf1f17a6b92055dfafde949c174fe1b3
MiguelAGutierrezS <gutierrezsantallamiguelangel@gmail.com>
2025-05-02 19:02:40 -0600

    Nosotros
    añadiendo la pagina de nosotros (lo puse en components

--- src/components/nosotros/Nosotros.jsx
+++ src/components/nosotros/Nosotros.jsx
@@ -14,45 +14,56 @@ const Nosotros = () => {
         { id: 4, nombre: "Nombre Miembro 4", foto: fotoRectangulo },
         { id: 5, nombre: "Nombre Miembro 5", foto: fotoRectangulo },
     ];
+    const galeria = [
+        { id: 1, foto: fotoRedondo },
+        { id: 2, foto: fotoRedondo },
+        { id: 3, foto: fotoRedondo },
+        { id: 4, foto: fotoRedondo },
+        { id: 5, foto: fotoRedondo },
+        { id: 6, foto: fotoRedondo },
+    ];
 
     return (
-        <section className="nosotros-container">
-            <section id="historia" className="seccion">
-                <h2>¿Quienes somos?</h2>
-                <p>
-                    OPEN FIBER es una comunidad global que une saberes ancestrales y tecnologías abiertas para transformar botellas plásticas en fibras reutilizables. Nacimos con la convicción de que el conocimiento debe compartirse y que las soluciones sostenibles pueden surgir desde cualquier rincón del mundo.
-                    Inspirados por la cultura guaraní isoseña y su profunda conexión con la vida, tejemos una red de colaboración entre makers, Fab Labs, diseñadores, investigadoras y comunidades que buscan una nueva forma de crear, reciclar y cuidar el planeta.
-                    Desarrollamos y compartimos herramientas de código abierto, como Arakuaa, una máquina accesible y fácil de construir que permite generar fibras textiles a partir de botellas PET, integrando prácticas ancestrales con tecnología moderna.
-                    Creemos en el poder de la colaboración, en la creatividad colectiva y en el valor de lo hecho con conciencia. Somos un puente entre tradición e innovación, entre la tierra y la tecnología, entre la historia y el futuro.
+        <>
 
-                </p>
-            </section>
-
-            <section id="vision" className="mitad-seccion">
-                <img src={vision} alt="Ilustración de vision" className="ilustracion" />
-                <div className="contenedor-titulo-descripcion">
-                    <h2>visión</h2>
+            <section className="nosotros-container-informacion">
+                <section id="historia" className="seccion">
+                    <h2>¿Quienes somos?</h2>
                     <p>
-                        Crear una comunidad global de innovación colaborativa que impulse el desarrollo, la mejora y la expansión de tecnologías sostenibles de reciclaje de botellas PET en fibras reutilizables. Promovemos soluciones abiertas y accesibles que permitan transformar residuos plásticos en recursos útiles, conectando saberes ancestrales con herramientas de fabricación digital.
-                    </p>
-                </div>
-            </section>
+                        OPEN FIBER es una comunidad global que une saberes ancestrales y tecnologías abiertas para transformar botellas plásticas en fibras reutilizables. Nacimos con la convicción de que el conocimiento debe compartirse y que las soluciones sostenibles pueden surgir desde cualquier rincón del mundo.
+                        Inspirados por la cultura guaraní isoseña y su profunda conexión con la vida, tejemos una red de colaboración entre makers, Fab Labs, diseñadores, investigadoras y comunidades que buscan una nueva forma de crear, reciclar y cuidar el planeta.
+                        Desarrollamos y compartimos herramientas de código abierto, como Arakuaa, una máquina accesible y fácil de construir que permite generar fibras textiles a partir de botellas PET, integrando prácticas ancestrales con tecnología moderna.
+                        Creemos en el poder de la colaboración, en la creatividad colectiva y en el valor de lo hecho con conciencia. Somos un puente entre tradición e innovación, entre la tierra y la tecnología, entre la historia y el futuro.
 
-            <section id="mision" className="mitad-seccion">
-                <div className="contenedor-titulo-descripcion">
-                    <h2>misión</h2>
+                    </p>
+                </section>
+
+                <section id="vision" className="mitad-seccion">
+                    <img src={vision} alt="Ilustración de vision" className="ilustracion" />
+                    <div className="contenedor-titulo-descripcion">
+                        <h2>visión</h2>
+                        <p>
+                            Crear una comunidad global de innovación colaborativa que impulse el desarrollo, la mejora y la expansión de tecnologías sostenibles de reciclaje de botellas PET en fibras reutilizables. Promovemos soluciones abiertas y accesibles que permitan transformar residuos plásticos en recursos útiles, conectando saberes ancestrales con herramientas de fabricación digital.
+                        </p>
+                    </div>
+                </section>
+
+                <section id="mision" className="mitad-seccion">
+                    <div className="contenedor-titulo-descripcion">
+                        <h2>misión</h2>
+                        <p>
+                            Convertirnos en la red global líder en tecnología de reciclaje de botellas PET, donde comunidades, Fab Labs y makers de todo el mundo puedan compartir investigaciones, mejoras y aplicaciones, creando un ecosistema dinámico de conocimiento abierto que impacte positivamente en el medio ambiente, la moda y la cultura.
+                        </p>
+                    </div>
+                    <img src={mision} alt="Ilustración de mision" className="ilustracion" />
+                </section>
+
+                <section id="objetivo" className="seccion">
+                    <h2>objetivo</h2>
                     <p>
-                        Convertirnos en la red global líder en tecnología de reciclaje de botellas PET, donde comunidades, Fab Labs y makers de todo el mundo puedan compartir investigaciones, mejoras y aplicaciones, creando un ecosistema dinámico de conocimiento abierto que impacte positivamente en el medio ambiente, la moda y la cultura.
+                        Consolidar una comunidad internacional de colaboración abierta dedicada al desarrollo, mejora continua y difusión de tecnologías de reciclaje de botellas PET. Facilitamos el acceso a herramientas de código abierto para transformar residuos en fibras sostenibles, promoviendo la investigación compartida y el compromiso ambiental en la industria textil.
                     </p>
-                </div>
-                <img src={mision} alt="Ilustración de mision" className="ilustracion" />
-            </section>
-
-            <section id="objetivo" className="seccion">
-                <h2>objetivo</h2>
-                <p>
-                    Consolidar una comunidad internacional de colaboración abierta dedicada al desarrollo, mejora continua y difusión de tecnologías de reciclaje de botellas PET. Facilitamos el acceso a herramientas de código abierto para transformar residuos en fibras sostenibles, promoviendo la investigación compartida y el compromiso ambiental en la industria textil.
-                </p>
+                </section>
             </section>
 
             <section className="equipo-container">
@@ -70,16 +81,14 @@ const Nosotros = () => {
             <section id="colaboraciones" className="seccion galeria">
                 <h2>Galería</h2>
                 <div className="fotos">
-                    <img src={fotoRedondo} alt="Foto 1" className="foto" />
-                    <img src={fotoRedondo} alt="Foto 2" className="foto" />
-                    <img src={fotoRedondo} alt="Foto 3" className="foto" />
-                    <img src={fotoRedondo} alt="Foto 4" className="foto" />
-                    <img src={fotoRedondo} alt="Foto 5" className="foto" />
-                    <img src={fotoRedondo} alt="Foto 6" className="foto" />
+                    {
+                        galeria.map((galeria, index) => {
+                            return <img key={index} src={galeria.foto} alt="Foto" className="foto" />
+                        })
+                    }
                 </div>
             </section>
-
-        </section>
+        </>
     );
 };
 
